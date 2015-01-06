547fdaa8d2127023537064096a1f986d8a795b4d
Lutz Latta <lutz@latta.de>
2015-01-06 00:20:01 -0800

    Minor auto format cleanup

--- src/Adafruit_TFTLCD/Adafruit_TFTLCD.h
+++ src/Adafruit_TFTLCD/Adafruit_TFTLCD.h
@@ -8,24 +8,22 @@
 #include <mraa/gpio.h>
 
 
-class Adafruit_TFTLCD : public Adafruit_GFX {
-
 public:
 
 	Adafruit_TFTLCD(void);
 
-	void     begin(uint16_t id);
-	void     drawPixel(int16_t x, int16_t y, uint16_t color);
-	void     drawFastHLine(int16_t x0, int16_t y0, int16_t w, uint16_t color);
-	void     drawFastVLine(int16_t x0, int16_t y0, int16_t h, uint16_t color);
-	void     fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t c);
-	void     fillScreen(uint16_t color);
-	void     reset(void);
-	void     setRegisters8(uint8_t *ptr, uint8_t n);
-	void     setRegisters16(uint16_t *ptr, uint8_t n);
-	void     setRotation(uint8_t x);
-	void     setAddrWindow(int x1, int y1, int x2, int y2);
-	void     pushColors(uint16_t *data, uint32_t len, bool first);
+	void begin(uint16_t id);
+	void drawPixel(int16_t x, int16_t y, uint16_t color);
+	void drawFastHLine(int16_t x0, int16_t y0, int16_t w, uint16_t color);
+	void drawFastVLine(int16_t x0, int16_t y0, int16_t h, uint16_t color);
+	void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t c);
+	void fillScreen(uint16_t color);
+	void reset(void);
+	void setRegisters8(uint8_t *ptr, uint8_t n);
+	void setRegisters16(uint16_t *ptr, uint8_t n);
+	void setRotation(uint8_t x);
+	void setAddrWindow(int x1, int y1, int x2, int y2);
+	void pushColors(uint16_t *data, uint32_t len, bool first);
 
 	uint16_t color565(uint8_t r, uint8_t g, uint8_t b);
 	uint16_t readPixel(int16_t x, int16_t y);
@@ -50,7 +48,7 @@ private:
 	void setLR(void);
 	void flood(uint16_t color, uint32_t len);
 
-	uint8_t  driver;
+	uint8_t driver;
 
 	mraa_gpio_context m_csPinCtx;
 	mraa_gpio_context m_cdPinCtx;
